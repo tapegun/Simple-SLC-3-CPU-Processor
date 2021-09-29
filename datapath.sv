45b8f9e5547913b065b6672f2fa9c4795d8c36aa
ssbonde2 <ssbonde2@illinois.edu>
2021-09-28 23:31:07 -0500

    stuff done

--- datapath.sv
+++ datapath.sv
@@ -1,1 +1,11 @@
-module datapath()
+module datapath(
+					input logic 
+					SR2, SR1,
+					DR,
+					ADDR1MUX, ADDR2MUX,
+					LD_REG, LD_BEN, LD_CC, LD_IR, LD_PC,
+					GateALU, GatePC, GateMARMUX, GateMDR,
+					output
+					);
+					
+register PC()
---
+++ register.sv
@@ -0,0 +1,14 @@
+module register (input logic [15:0] din,
+	input logic clk, load, reset,
+	output logic [15:0] dout
+);
+
+   always_ff @ (posedge clk)
+	begin
+		if(reset)
+			dout <= 16'h00;
+		else if(load)
+			dout <= din;
+	end
+
+endmodule 
