59cb7a51a1aaa35a18fdbe00caa6fe2d35a26cf4
Chavez, Jorge <jorgejc2@illinois.edu>
2021-09-28 21:24:46 -0500

    muxes

---
+++ 2to1Mux3.sv
@@ -0,0 +1,11 @@
+module 2to1Mux3 (
+                input logic [2:0] A, B, //these are the 3 bit inputs
+                input logic S,          //the select bit
+                output logic [2:0]  OUT //the output
+);
+
+always_comb begin
+    OUT =  B;
+    if (S):
+        OUT = A;
+end
---
+++ 4to1Mux16.sv
@@ -0,0 +1,14 @@
+module 4to1Mux16 (
+                input logic [15:0] A, B, C, D, //inputs
+                input logic [1:0] S,            //select input
+                output logic OUT                //16 bit output
+);
+
+always_comb begin
+    case (S):
+        2'b00: OUT = A;
+        2'b01: OUT = B;
+        2'b10: OUT = C:
+        2'b11: OUT = D:
+    endcase
+end
