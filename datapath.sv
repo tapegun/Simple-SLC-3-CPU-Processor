cb98ad2a95a1b8329e76a2517b710ec6effc76d7
ssbonde2 <ssbonde2@illinois.edu>
2021-09-29 00:20:14 -0500

    datapath connections

--- datapath.sv
+++ datapath.sv
@@ -1,11 +1,49 @@
 module datapath(
-					input logic 
-					SR2, SR1,
-					DR,
-					ADDR1MUX, ADDR2MUX,
-					LD_REG, LD_BEN, LD_CC, LD_IR, LD_PC,
-					GateALU, GatePC, GateMARMUX, GateMDR,
-					output
-					);
-					
-register PC()
+	input logic clk, reset
+	SR2, SR1,
+	DR,
+	LD_REG, LD_BEN, LD_CC, LD_IR, LD_PC, LD_MAR, LD_MDR, LD_LED
+	GateALU, GatePC, GateMARMUX, GateMDR,
+	ADDR1MUX, input logic [1:0] ADDR2MUX,
+	input logic[1:0] ALUK,
+	input logic MEMIO,
+	input logic [15:0] MDR_In,
+	output logic BEN,
+	output logic [15:0] IR_OUT, PC_OUT, MAR_OUT, MDR_OUT,
+	output logic [11:0] LED
+	);
+
+	logic [15:0] bus;
+	
+	logic [15:0] PCMUXOUT;
+	logic [15:0] PCOUT;
+	logic [15:0] IROUT;
+
+	logic [15:0] IRSEXT10;
+	logic [15:0] IRSEXT8;
+	logic [15:0] IRSEXT5;
+	logic [15:0] IRSEXT4;
+	
+	logic [15:0] ADDR2MUXOUT;
+	logic [15:0] ADDR1MUXOUT;
+
+	logic [15:0] SR1_OUT;
+	logic [15:0] SR2_OUT;
+	
+	logic [15:0] SR2MUXOUT;
+
+	logic [15:0] ALUOUT;
+		
+	logic [15:0] MDRMUXOUT;
+
+	register PC();
+
+	register IR();
+
+	REGFILE  regs();
+
+	ALU  	 ALU();	
+
+endmodule
+
+
